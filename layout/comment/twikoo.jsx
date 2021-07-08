830f889a7dadf5ea378539dc0c12ea936d9073ae
iMaeGoo <mail1st@qq.com>
2021-07-08 17:56:25 +0800

    support comment image preview

--- layout/comment/twikoo.jsx
+++ layout/comment/twikoo.jsx
@@ -8,7 +8,31 @@ class Twikoo extends Component {
       jsUrl,
     } = this.props;
     const js = `twikoo.init({
-      envId: '${envId}'
+      envId: '${envId}',
+      onCommentLoaded: function () {
+        var commentContents = document.getElementsByClassName('tk-content');
+        for (var i = 0; i < commentContents.length; i++) {
+          var commentItem = commentContents[i];
+          var imgEls = commentItem.getElementsByTagName('img');
+          if (imgEls.length > 0) {
+            for (var j = 0; j < imgEls.length; j++) {
+              var imgEl = imgEls[j];
+              var aEl = document.createElement('a');
+              aEl.setAttribute('class', 'tk-lg-link');
+              aEl.setAttribute('href', imgEl.getAttribute('src'));
+              aEl.setAttribute('data-src', imgEl.getAttribute('src'));
+              aEl.appendChild(imgEl.cloneNode(false));
+              imgEl.parentNode.insertBefore(aEl, imgEl.nextSibling);
+              imgEl.remove();
+            }
+            if (typeof $.fn.lightGallery === 'function') {
+              $(commentItem).lightGallery({
+                selector: '.tk-lg-link'
+              });
+            }
+          }
+        }
+      }
     });`;
     return (
       <Fragment>
