1600098cbdb6285fbed40f4b255d7d443de0b0dc
iMaeGoo <mail1st@qq.com>
2023-03-09 09:47:58 +0800

    fix: cannot load plugin "netlify"

--- layout/layout.jsx
+++ layout/layout.jsx
@@ -48,8 +48,6 @@ module.exports = class extends Component {
                 <script type="text/javascript" src="/js/imaegoo/imaegoo.js"></script>
                 <script type="text/javascript" src="/js/imaegoo/universe.js"></script>
                 {/* <script type="text/javascript" src="/js/live2d/autoload.js"></script> */}
-                {config.plugins.netlify ? <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script> : null}
-                {config.plugins.netlify ? <script src="/js/imaegoo/netlify.js"></script> : null}
             </body>
         </html>;
     }
